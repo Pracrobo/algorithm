8d1caa2fd7c1ccf5c1794a31ede6f273c273b2f6
Pracrobo <cpu180621@gmail.com>
2025-02-26 18:42:42 +0900

    BFS/DFS

--- algorithm.md
+++ algorithm.md
@@ -25,3 +25,6 @@
 # 참고자료 및 강의
 - Youtube
 - Udemy(컴공선배와 함께하는 알고리즘 코딩 테스트 대비 전략)
+- https://wikidocs.net/book/1
+- https://wikidocs.net/book/1553
+- https://wikidocs.net/book/2184
---
+++ data_structure/DFS_ex.py
@@ -0,0 +1,14 @@
+adj =[[0] * 13 for _ in range(13)]
+adj[0][1] = adj[0][7] = 1
+
+for row in adj:
+    print(row)
+
+
+
+def dfs(now):
+    for nxt in range(13):
+        if adj[now][nxt]:
+            dfs[nxt]
+
+dfs(0)
