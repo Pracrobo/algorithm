cd456066479233e00cec4a1a5a843d541983550f
Pracrobo <cpu180621@gmail.com>
2025-02-27 23:29:45 +0900

    ã백트ë랴ë래í키í킹 문제

---
+++ backtracking/S_BackTracking.py
@@ -0,0 +1,32 @@
+# 정점 노드 N
+# 간선 선 갯수 M
+# 1<= N <=1,000
+# 0<= M <=N x (N-1)/2
+import sys
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+N,M = map(int, input().split())
+adj = [[0] * N for _ in range(N)]
+for i in range(M):
+    u, v = map(lambda x: x-1, map(int, input().split()))
+    adj[u][v] = adj[v][u] = 1 # 무방향 그래프
+
+ans = 0  #연결요소갯수
+chk = [False] * N
+
+
+def dfs(now):
+    for nxt in range(N):
+        if adj[now][nxt] and not chk[nxt]:
+            chk[nxt] = True #  체크를 먼저해주는게 메모리 낭비 줄임
+            dfs(nxt)
+
+for i in range(N):
+    if not chk[i]:
+        ans += 1
+        chk[i] = True
+        dfs(i)
+
+
+print(ans)
