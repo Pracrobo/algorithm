33e78b376cc4c2d0ffb3074bab9af5deea53eea0
Pracrobo <cpu180621@gmail.com>
2025-03-06 22:11:05 +0900

    알Binary Search

---
+++ binary_search/P_BS2.py
@@ -0,0 +1,17 @@
+from bisect import bisect_left, bisect_right
+
+N = int(input()) # _ = int(input())
+cards = sorted(map(int, input().split()))
+M = int(input()) # _ = int(input())
+match = list(map(int, input().split()))
+
+# 반복문 O(MN) : 500,000 * 500,000 = 250,000,000,000 (시간초과)
+# 이분탐색 : logN * M = 500,000 * M
+ans = []
+for m in match:
+    l = bisect_left(cards, m)
+    r = bisect_right(cards, m)
+    ans.append(1 if r - l > 0 else 0) # l if r - l else 0
+
+# print('', join(map(str, ans))
+print(*ans)
